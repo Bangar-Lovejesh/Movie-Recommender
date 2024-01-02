ef4da94975aa47bc6b5c5866163023205ed542a8
root <root@Bangar.localdomain>
2024-01-02 15:36:46 -0500

    bullshit

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/test")
+# @app.route('/test')
+# def test():
+#     return [{ "id":1,
+#               "name" : "Movie Name1"
+#     },{
+#         "id": 2,
+#         "name": "Movie Name2"
+#     },{
+#         "id": 3,
+#         "name": "Movie Name3"
+#     }]
+
+@app.route('/test', methods = ['GET','POST'], strict_slashes=False)
 def test():
-    return "It works"
+    name = "unavailable"
+    if request.method == 'POST':
+        print("inside")
+        name = request.get_json()['name'];
+
+        return [{ "id":1,
+                  "name" : name
+        },{
+            "id": 2,
+            "name": name
+        },{
+            "id": 3,
+            "name": name
+        }]
+
+    return [{"id": 1,
+             "name": name
+             }, {
+                "id": 2,
+                "name": name
+            }, {
+                "id": 3,
+                "name": name
+            }]
 
 
-if __name__ == "__main__":
+
+
+
+if __name__ == '__main__':
     app.run()
+
--- movie-ui/src/test.js
+++ movie-ui/src/test.js
@@ -1,14 +1,43 @@
 import {useLocation, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function Test(){
     const location = useLocation();
     const nav = useNavigate();
-    const data = location.state;
+    const post = location.state;
+    const [stuff,setStuff] = useState([]);
+
+    useEffect(() => {
+        fetch("/test",{
+            method:'POST',
+            headers:{ "Content-Type": "application/json" },
+            body: JSON.stringify({"name":post})
+        }).then((res)=>
+            res.json().then((data)=>setStuff(data)))
+    }, []);
+
+    // useEffect(()=>{
+    //     fetch("/test").then((res)=> {
+    //         return res.json().then((data) => {
+    //             setStuff(data);
+    //             // console.log(data);
+    //         });
+    //     })
+    // },[]);
+
+    const listItems = stuff.map((i)=>
+        <li key = {i.id}>{i.name}</li>
+    );
+
 
     return (
         <>
-            <p>{data}</p>
+            <div className="App">
+      <header className="App-header">
+            <ul>{listItems}</ul>
             <button type="submit" onClick={() => nav('/')}>Go back</button>
+      </header></div>
+
         </>
     )
 }
