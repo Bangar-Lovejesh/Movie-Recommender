91df8571d55cecd92a1ae0234663b428db745784
bangar-lovejesh <lovejesh.bangar@gmail.com>
2024-03-07 12:18:18 -0500

    fixing some issues

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,44 +1,34 @@
 import os
-
-<<<<<<< HEAD
 from dotenv import load_dotenv
-from sqlalchemy import text
-from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+import numpy as np
+from sqlalchemy.types import ARRAY, Integer, String
 
 load_dotenv("../.env")
-os.environ['DATABASE_URL'] = "postgresql://lovejesh:7dJx7gKdxrJ9QLaX3c-tPg@tangy-raven-13112.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
-def main():
-    db_uri = os.environ['DATABASE_URL']
-=======
-from sqlalchemy import create_engine, text
-
-
 def main():
     db_uri = os.environ["DATABASE_URL"]
->>>>>>> aw/db_notebook
     try:
-        print("here")
-        engine = create_engine(db_uri)
+        print(os.environ["DATABASE_URL"])
+        engine = create_engine(os.environ["DATABASE_URL"])
         print(engine.connect())
-        print("here")
+        result = []
         with engine.connect() as con:
-<<<<<<< HEAD
-            result = con.execute(text("SELECT title from movie_scores limit 10"))
-            print("here")
-            print(result)
+            result = con.execute(text("SELECT genre from movies_scores where id = '96882' ")).fetchall()
+        arr = np.array(result[0][0])
+        best_result = np.argsort(arr)[-1:-10:-1]
+        print(len(result[0][0]))
+        query_arg=[]
+        for i in best_result:
+            query_arg.append(str(i))
+        query_arg = tuple(query_arg)
+        print(text("SELECT id from netflix where id in :x "),{"x":query_arg})
+        with engine.connect() as con:
+            n_movies = (con.execute(text("SELECT title from netflix where netflix.index in :x "),{"x":query_arg})).fetchall()
+            # print("here")
+            print(n_movies)
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
     main()
-=======
-            result = con.execute(text("SELECT title from movies_scores limit 10"))
-            print("here")
-            print(result.all())
-    except Exception as e:
-        print(e)
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> aw/db_notebook
